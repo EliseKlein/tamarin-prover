27da9b6078bc4208d37fc617793116b50adac329
Lucca Hirschi <lucca.hirschi@inf.ethz.ch>
2017-11-23 18:14:44 +0100

    Script to create TAR + remove stateless LAK

---
+++ packPOST18.sh
@@ -0,0 +1,2 @@
+tar -cf  XOR_POST18_tamarinAndCaseStudies.tar examples/post18 README.md stack.yaml tamarin-prover.cabal Setup.hs Makefile LICENSE src AUTHORS ghci-dev.sh etc data lib images plugins
+tar -cf  XOR_POST18_caseStudies.tar examples/post18/*
