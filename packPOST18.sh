b0b301a821da291551130c06bd9257ebca52620a
Lucca Hirschi <lucca.hirschi@inf.ethz.ch>
2018-01-29 18:31:36 +0100

    minor

--- packPOST18.sh
+++ packPOST18.sh
@@ -1,2 +1,2 @@
-zip -r  xor_submission_tamarinAndCaseStudies.zip examples/xor-submission README.md stack.yaml tamarin-prover.cabal Setup.hs Makefile LICENSE src AUTHORS ghci-dev.sh etc data lib images plugins
-zip -r  xor_submission_caseStudies.zip examples/xor-submission/*
+zip -r  xor-submission-tamarinAndCaseStudies.zip examples/xor-submission README.md stack.yaml tamarin-prover.cabal Setup.hs Makefile LICENSE src AUTHORS ghci-dev.sh etc data lib images plugins
+zip -r  xor-submission-caseStudies.zip examples/xor-submission/*
