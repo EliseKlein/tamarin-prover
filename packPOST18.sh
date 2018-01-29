0271720f8f01d350b143b9e9170ca9140eb448a6
Lucca Hirschi <lucca.hirschi@inf.ethz.ch>
2018-01-29 18:20:32 +0100

    update script for packing

--- packPOST18.sh
+++ packPOST18.sh
@@ -1,2 +1,2 @@
-zip -r  xor_submission_tamarinAndCaseStudies.zip examples/post18 README.md stack.yaml tamarin-prover.cabal Setup.hs Makefile LICENSE src AUTHORS ghci-dev.sh etc data lib images plugins
-zip -r  xor_submission_caseStudies.zip examples/post18/*
+zip -r  xor_submission_tamarinAndCaseStudies.zip examples/xor-submission README.md stack.yaml tamarin-prover.cabal Setup.hs Makefile LICENSE src AUTHORS ghci-dev.sh etc data lib images plugins
+zip -r  xor_submission_caseStudies.zip examples/xor-submission/*
