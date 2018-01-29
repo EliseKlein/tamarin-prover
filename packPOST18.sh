434adbd15bbef8ca88554efd5d4d03873f853a27
Lucca Hirschi <lucca.hirschi@inf.ethz.ch>
2018-01-29 17:33:41 +0100

    update arxiv naming

--- packPOST18.sh
+++ packPOST18.sh
@@ -1,2 +1,2 @@
-zip -r  XOR_POST18_tamarinAndCaseStudies.zip examples/post18 README.md stack.yaml tamarin-prover.cabal Setup.hs Makefile LICENSE src AUTHORS ghci-dev.sh etc data lib images plugins
-zip -r  XOR_POST18_caseStudies.zip examples/post18/*
+zip -r  xor_submission_tamarinAndCaseStudies.zip examples/post18 README.md stack.yaml tamarin-prover.cabal Setup.hs Makefile LICENSE src AUTHORS ghci-dev.sh etc data lib images plugins
+zip -r  xor_submission_caseStudies.zip examples/post18/*
