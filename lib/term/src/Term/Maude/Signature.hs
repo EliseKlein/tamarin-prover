10f6faaa62b28f2c28f9f0371f07e3ee837775ef
Elise Klein <elise.klein@inria.fr>
2025-03-07 15:42:37 +0100

    Correcting position

--- lib/term/src/Term/Maude/Signature.hs
+++ lib/term/src/Term/Maude/Signature.hs
@@ -232,7 +232,7 @@ prettyMaudeSigExcept sig excl = P.vcat
     , ppNonEmptyList' "functions:" ppFunSymb $ (S.toList (S.map (\fct -> (NoEqUser fct)) (stFunSyms sig S.\\ exclNoEq)) ++ S.toList (S.map (\fct -> (ACfctUser fct)) (stACFunSyms sig S.\\ exclAC)) ++ [ACfctUser (BC.pack "add", (Public,Constructor)), NoEqUser (BC.pack "unionUD", (1,Public,Destructor))])
     , ppNonEmptyList
         (\ds -> P.sep (P.keyword_ "equations:" : map (P.nest 2) ds))
-        prettyCtxtStRule $ (S.toList (stRules sig) ++ [CtxtStRule (FAPP (NoEq (BC.pack "unionUD", (1,Public,Destructor))) [FAPP (AC (ACfct (BC.pack "add", (Public,Constructor)))) [LIT (Var (LVar "x"  LSortMsg   0)),LIT (Var (LVar "y"  LSortMsg   0))]]) (StRhs [[1,0]] (LIT (Var (LVar "x"  LSortMsg   0))))])
+        prettyCtxtStRule $ (S.toList (stRules sig) ++ [CtxtStRule (FAPP (NoEq (BC.pack "unionUD", (1,Public,Destructor))) [FAPP (AC (ACfct (BC.pack "add", (Public,Constructor)))) [LIT (Var (LVar "x"  LSortMsg   0)),LIT (Var (LVar "y"  LSortMsg   0))]]) (StRhs [[0,0]] (LIT (Var (LVar "x"  LSortMsg   0))))])
     ]
   where
     ppNonEmptyList' name     = ppNonEmptyList ((P.keyword_ name P.<->) . P.fsep)
